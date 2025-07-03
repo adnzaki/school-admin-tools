0fffde0d5d4874731f81915f7005c01bbff72af1
Adnan Zaki <adnzaki@outlook.com>
2025-07-03 14:59:27 +0700

    hide dark mode and color theme toggle

--- app/src/layout/AppTopbar.vue
+++ app/src/layout/AppTopbar.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { useLayout } from '@/layout/composables/layout';
-import AppConfigurator from './AppConfigurator.vue';
 
 const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
 </script>
@@ -36,10 +35,10 @@ const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
 
     <div class="layout-topbar-actions">
       <div class="layout-config-menu">
-        <button type="button" class="layout-topbar-action" @click="toggleDarkMode">
+        <!-- <button type="button" class="layout-topbar-action" @click="toggleDarkMode">
           <i :class="['pi', { 'pi-moon': isDarkTheme, 'pi-sun': !isDarkTheme }]"></i>
-        </button>
-        <div class="relative">
+        </button> -->
+        <!-- <div class="relative">
           <button
             v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
             type="button"
@@ -48,7 +47,7 @@ const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
             <i class="pi pi-palette"></i>
           </button>
           <AppConfigurator />
-        </div>
+        </div> -->
       </div>
 
       <button
