5b76541991f65b7f1b2841f0883b20114c55bca4
Adnan Zaki <adnzaki@outlook.com>
2025-07-22 11:49:33 +0700

    rebranding UI

--- app/src/layout/AppFooter.vue
+++ app/src/layout/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="layout-footer">
-    SAKAI by
-    <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+    Developed with ❤️ by
+    <p class="text-primary font-bold hover:underline">Adnan Zaki</p>
   </div>
 </template>
