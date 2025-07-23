c845e30c8cb5a9a4aa1568d52f51b02d7ce0a287
Adnan Zaki <adnzaki@outlook.com>
2025-07-23 10:21:19 +0700

    fix buttons layout

--- app/src/views/employee/ButtonMenu.vue
+++ app/src/views/employee/ButtonMenu.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="flex flex-col md:flex-row gap-8">
-    <div class="md:w-2/6">
+  <div class="flex flex-col gap-4 md:flex-col lg:flex-row">
+    <div class="w-full lg:w-2/4 flex flex-wrap gap-2">
       <div class="flex flex-col gap-4">
         <div class="flex flex-wrap">
           <Button :label="$t('common.buttons.add')" @click="showForm" icon="pi pi-plus" class="mr-2 mb-2"></Button>
           <Button :label="$t('common.buttons.delete')" icon="pi pi-trash" severity="secondary" class="mr-2 mb-2"></Button>
+          <Button label="Import Data" icon="pi pi-file-plus" severity="primary" outlined class="mr-2 mb-2"></Button>
         </div>
       </div>
     </div>
-    <div class="md:w-1/6">
-      <div class="flex flex-col gap-4">
-        <div class="flex flex-wrap">
-          <PagingRows :selected-row="25" />
+    <div class="w-full lg:w-2/4 flex flex-col md:flex-row gap-4">
+      <div class="md:w-1/3">
+        <div class="flex flex-col gap-4">
+          <div class="flex flex-wrap">
+            <PagingRows :selected-row="25" />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="md:w-3/6">
-      <div class="flex flex-col gap-4">
-        <div class="flex flex-wrap">
-          <SearchBox :paging="paging" v-model="paging.state.search" class="w-full" :placeholder="$t('common.buttons.searchByName')" />
+      <div class="md:w-2/3">
+        <div class="flex flex-col gap-4">
+          <div class="flex flex-wrap">
+            <SearchBox :paging="paging" v-model="paging.state.search" class="w-full" :placeholder="$t('common.buttons.searchByName')" />
+          </div>
         </div>
       </div>
     </div>
