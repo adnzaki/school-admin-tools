f1b9a3d214df2cb56eb0037403e0928e571f2813
Adnan Zaki <adnzaki@outlook.com>
2025-12-18 15:58:37 +0700

    fix headmaster sign position and add custom margin-left

--- api/app/Views/layout/headmaster-sign.php
+++ api/app/Views/layout/headmaster-sign.php
@@ -4,12 +4,12 @@ $headmasterNip = $institusi['nip_kepala_sekolah'];
 
 ?>
 <?php if (! isset($useSignature)) : ?>
-    <div style="margin-left: 50%;" class="<?= $signFontSize ?? 'font-11' ?>">
-        <p class="text-center">
+    <div style="margin-left: <?= $marginLeft ?? '60%' ?>;" class="<?= $signFontSize ?? 'font-11' ?>">
+        <p>
             Bekasi, <?= $date ?><br />
             Kepala Sekolah
         </p>
-        <p class="text-center" style="margin-top: 80px;">
+        <p style="margin-top: 80px;">
             <strong><?= $headmaster ?></strong><br />
             NIP. <?= formatNIP($headmasterNip) ?>
         </p>
